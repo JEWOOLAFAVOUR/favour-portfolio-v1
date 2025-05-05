8177b447e193e6ef6c14aaf510cdd38439ad3b35
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2025-05-05 18:26:31 +0100

    sksk

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,16 @@
-import React, { useEffect } from 'react';
-import { createBrowserRouter, RouterProvider, useLocation, Navigate } from "react-router-dom";
-import HomePage from '../screen/HomePage';
-import ProtectedRoute from './protectedRoute';
-import useAuthStore from '../store/useAuthStore';
-
-
+import { useEffect } from "react"
+import { createBrowserRouter, RouterProvider, useLocation } from "react-router-dom"
+import HomePage from "../screen/HomePage"
+import NotFound from "../screen/NotFound" // Import the NotFound component
 
 const ScrollToTop = () => {
-    const { pathname } = useLocation();
+    const { pathname } = useLocation()
 
     useEffect(() => {
-        window.scrollTo(0, 0);
-    }, [pathname]);
+        window.scrollTo(0, 0)
+    }, [pathname])
 
-    return null;
+    return null
 }
 
 const RouteWrapper = ({ Component }) => (
@@ -21,39 +18,84 @@ const RouteWrapper = ({ Component }) => (
         <ScrollToTop />
         <Component />
     </>
-);
+)
 
 const RootRouteHandler = () => {
-    const { isAuthenticated, token } = useAuthStore();
-
-    // If user is already authenticated, redirect to dashboard/cbt
-    // if (isAuthenticated && token) {
-    //     return <Navigate to="/dashboard/cbt" replace />;
-    // }
-
-    // Otherwise show the homepage
-    return <HomePage />;
-};
-
-
+    return <HomePage />
+}
 
 const Routes = () => {
     const pageRoutes = [
         {
-            path: "",
+            path: "/",
             element: <RouteWrapper Component={RootRouteHandler} />,
         },
+        // Add your other routes here
 
-    ];
+        // Add the 404 route as the last route to catch all unmatched paths
+        {
+            path: "*",
+            element: <RouteWrapper Component={NotFound} />,
+        },
+    ]
+
+    const router = createBrowserRouter([...pageRoutes])
+
+    return <RouterProvider router={router} />
+}
+
+export default Routes
+
+
+// import React, { useEffect } from 'react';
+// import { createBrowserRouter, RouterProvider, useLocation, Navigate } from "react-router-dom";
+// import HomePage from '../screen/HomePage';
+// import ProtectedRoute from './protectedRoute';
+// import useAuthStore from '../store/useAuthStore';
 
 
 
-    const router = createBrowserRouter([
-        ...pageRoutes,
-    ]);
+// const ScrollToTop = () => {
+//     const { pathname } = useLocation();
 
-    return <RouterProvider router={router} />;
-};
+//     useEffect(() => {
+//         window.scrollTo(0, 0);
+//     }, [pathname]);
 
-export default Routes;
+//     return null;
+// }
+
+// const RouteWrapper = ({ Component }) => (
+//     <>
+//         <ScrollToTop />
+//         <Component />
+//     </>
+// );
+
+// const RootRouteHandler = () => {
+
+//     return <HomePage />;
+// };
+
+
+
+// const Routes = () => {
+//     const pageRoutes = [
+//         {
+//             path: "",
+//             element: <RouteWrapper Component={RootRouteHandler} />,
+//         },
+
+//     ];
+
+
+
+//     const router = createBrowserRouter([
+//         ...pageRoutes,
+//     ]);
+
+//     return <RouterProvider router={router} />;
+// };
+
+// export default Routes;
 
