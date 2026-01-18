1d143b50497ff87f1c8ed7cbc6cb31a57e62fe74
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2026-01-19 00:21:47 +0100

    refactor: remove unused components and clean up codebase
    - Deleted Contact component and its associated logic.
    - Removed EmailSidebar, Experience, Footer, Hero, Loader, Navbar, Projects, SocialSidebar components.
    - Cleaned up the authentication store by removing unused state management.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,10 +5,7 @@ import {
   useLocation,
 } from "react-router-dom";
 import App from "../App";
-import ChallengePage from "../screen/ChallengePage";
-import PDFViewerPage from "../screen/PDFViewerPage";
-import NotFound from "../screen/NotFound"; // Import the NotFound component
-import ArchivePage from "../screen/ArchivePage"; // Import the ArchivePage component
+import ArchivePage from "../screen/ArchivePage";
 
 const ScrollToTop = () => {
   const { pathname } = useLocation();
@@ -38,23 +35,8 @@ const Routes = () => {
       element: <RouteWrapper Component={RootRouteHandler} />,
     },
     {
-      path: "/challenge",
-      element: <RouteWrapper Component={ChallengePage} />,
-    },
-    {
-      path: "/challenge/:pdfName",
-      element: <RouteWrapper Component={PDFViewerPage} />,
-    },
-    {
       path: "/archive",
       element: <RouteWrapper Component={ArchivePage} />,
-    },
-    // Add your other routes here
-
-    // Add the 404 route as the last route to catch all unmatched paths
-    {
-      path: "*",
-      element: <RouteWrapper Component={NotFound} />,
     },
   ];
 
@@ -64,49 +46,3 @@ const Routes = () => {
 };
 
 export default Routes;
-
-// import React, { useEffect } from 'react';
-// import { createBrowserRouter, RouterProvider, useLocation, Navigate } from "react-router-dom";
-// import HomePage from '../screen/HomePage';
-// import ProtectedRoute from './protectedRoute';
-// import useAuthStore from '../store/useAuthStore';
-
-// const ScrollToTop = () => {
-//     const { pathname } = useLocation();
-
-//     useEffect(() => {
-//         window.scrollTo(0, 0);
-//     }, [pathname]);
-
-//     return null;
-// }
-
-// const RouteWrapper = ({ Component }) => (
-//     <>
-//         <ScrollToTop />
-//         <Component />
-//     </>
-// );
-
-// const RootRouteHandler = () => {
-
-//     return <HomePage />;
-// };
-
-// const Routes = () => {
-//     const pageRoutes = [
-//         {
-//             path: "",
-//             element: <RouteWrapper Component={RootRouteHandler} />,
-//         },
-
-//     ];
-
-//     const router = createBrowserRouter([
-//         ...pageRoutes,
-//     ]);
-
-//     return <RouterProvider router={router} />;
-// };
-
-// export default Routes;
