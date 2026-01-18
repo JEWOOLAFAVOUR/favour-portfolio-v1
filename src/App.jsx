3308f096860e99da5d182e8c69894bf990d3f324
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2026-01-18 03:54:15 +0100

    Clean up portfolio - simple greeting page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import Routes from "./routes";
-import { ToastContainer } from "./components/utilis";
 import { ThemeProvider } from "./components/template/ThemeProvider";
-
 
 export default function App() {
   return (
-    <>
-      <ThemeProvider>
-        <ToastContainer />
-        <Routes />
-      </ThemeProvider>
-    </>
+    <ThemeProvider>
+      <div className="flex items-center justify-center min-h-screen bg-white dark:bg-black">
+        <div className="text-center">
+          <h1 className="text-5xl font-bold text-black dark:text-white">
+            Hi, my name is Favour
+          </h1>
+          <p className="mt-4 text-xl text-gray-600 dark:text-gray-400">
+            Portfolio Coming Soon...
+          </p>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
